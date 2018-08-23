f20557c9ee04b07b4f5081a4f7960a256d5e06e3
Matthew Mackay <matthewmackay26@hotmail.com>
2018-08-23 11:12:37 -0700

    Added picture gallery

--- javascript/examples.js
+++ javascript/examples.js
@@ -4,17 +4,32 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function generatePi(){
-    for (let i = 0; i < 10000; i++){
-        let currentPi = 0;
-        let oscillation = 1;
-        currentPi = oscillation * (4/(2*i + 1));
+async function generatePi(){
+    let currentPi = 0;
+    let oscillation = 1; 
+    for (let i = 0; i < 100000000; i++){
+        currentPi += oscillation * (4/(2*i + 1));
         oscillation *= -1;
         if (i % 50 == 49){
-            displayPi(currentPi);
+            displayPi(currentPi.toFixed(20));
         }
+        await(sleep(5));
     }
 }
 function displayPi(data){
-    document.getElementById("displayAsyncMath").innerHTML = data;
+    document.getElementById("displayAsyncMath").innerHTML = "PI: " + data;
+}
+
+async function scrollingPictures(){
+    let gallery = document.getElementById('pictureGallery1');
+    let pictures = ['converse.jpeg', 'mountain.jpeg', 'forest.jpeg'];
+    let currentPicture = 0;
+    gallery.appendChild(document.createElement('img'));
+    while(true){
+        let image = document.createElement('img');
+        image.src = 'pictures/' + pictures[currentPicture];
+        gallery.replaceChild(image, gallery.childNodes[0]);
+        await(sleep(1000));
+        currentPicture = (currentPicture+1)%pictures.length;
+    }
 }

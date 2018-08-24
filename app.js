9950b33fb6a5c325f24dddcd9f0e432a441b29fa
Matthew Mackay <matthewmackay26@hotmail.com>
2018-08-24 13:34:59 -0700

    Added Javascript for Node.js

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-//Load express module with `require` directive
-let express = require('express')
-let app = express()
+let http = require('http');  
+let url = require('url');  
+let fs = require('fs');
+let express = require('express');
+let app = express();
+let path = require('path')
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(express.static('css'));
+app.use(express.static('pictures'));
+app.use(express.static('javascript'));
 
-//Launch listening server on port 8084
-app.listen(8084, function () {
-  console.log('App listening on port 8084!')
-})
+let server = http.createServer(function(req, res) {  
+    if(req.url === "/"){
+      fs.readFile("./index.html", "UTF-8", function(err, html){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+      });
+    }
+    else if(req.url.match("\.css$")){
+      let cssPath = path.join(__dirname, req.url);
+      let fileStream = fs.createReadStream(cssPath, "UTF-8");
+      res.writeHead(200, {"Content-Type": "text/css"});
+      fileStream.pipe(res);
+    }
+    else if(req.url.match("\.jpeg$")){
+      let imagePath = path.join(__dirname, req.url);
+      let fileStream = fs.createReadStream(imagePath);
+      res.writeHead(200, {"Content-Type": "image/png"});
+      fileStream.pipe(res);
+    }
+    else if(req.url.match('\.js$')){
+      let jsPath = path.join(__dirname, req.url);
+      
+    }
+    else if(req.url.match("\.html$")){
+      let htmlPath = req.url;
+      fs.readFile('./'+htmlPath, 'UTF-8', function(err, html){
+        if(err){
+          console.log("Error parsing html page");
+        }
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+      });
+    }
+    else{
+      res.writeHead(404, {"Content-Type": "text/html"});
+      res.end("No Page Found");
+    }
+});  
+server.listen(8084);  
